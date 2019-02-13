890c01b74eaf6ed2ad37dd46000a5aa13f55f3c5
DianeDerycke <dianedhayat@gmail.com>
2019-02-13 20:31:49 +0100

    clean lexer.h

--- includes/lexer.h
+++ includes/lexer.h
@@ -50,18 +50,26 @@ typedef struct		s_opt
 	ssize_t			v;
 }					t_opt;
 
-//lex_input
-int     lex_input(t_param *param);
-char 	*get_valid_input(void);
-//def type char function
+//LEX_INIT
+t_param    	*init_param(void);
+void    	free_param(t_param *param);
+int         push_node(char *value, int token, t_ast **node);
+t_ast     	*create_elem(void);
+void        display_list(t_ast *lst);
+void        display_tree(t_ast *tree, int lvl, int position);
+
+//LEX UTILS ==> def type char function
+int         lex_is_special_char(int c);
 int         ft_isallowedsymb(int c);
 int         ft_is_operator(int c);
+int         ft_isidentifier(int c);
 int         ft_is_single_quote(int c);
 int         ft_is_double_quote(int c);
-int         ft_isidentifier(int c);
-int         lex_is_special_char(int c);
+int         get_ast_op(char *c, int length);
+char        *copy_until_ft(char *s, int *start, int(*f)(int c));
+char        *copy_until_array_ft(char *s, int *start, int(*array[2])(int));
 
-//lex_action
+//LEX_ACTION
 int     whitespace_action(t_param *param);
 int     single_quote_action(t_param *param);
 int     double_quote_action(t_param *param);
@@ -69,89 +77,72 @@ int     io_number_action(t_param *param);
 int     digit_action(t_param *param);
 int     operator_action(t_param *param);
 int     identifier_action(t_param *param);
-//token
-int         get_ast_op(char *c, int length);
 
-//lex_init
-t_param    *init_param(void);
-void    free_param(t_param *param);
+//LEX INPUT
+int     	lex_input(t_param *param);
+char 		*get_valid_input(void);
 
-// list function
-void        display_list(t_ast *lst);
-int         push_node(char *value, int token, t_ast **node);
-t_ast     *create_elem(void);
-
-//utils
-char        *copy_until_ft(char *s, int *start, int(*f)(int c));
-char        *copy_until_array_ft(char *s, int *start, int(*array[2])(int));
-
-//Quotes
-void		handle_quotes(char **input);
-int     is_valid_quotes(char *str);
-
-//ERROR CASE
-ssize_t     error_arg(void);
-int     	error_execution(char *cmd_name);
-
-
-//parser
-t_ast     *create_ast(t_ast *curr_node, t_ast *start, t_ast *end);
-
-//parser_utils
-void        display_tree(t_ast *tree, int lvl, int position);
-
-//parser_exec
-int     	exec_builtin(t_sh *shell);
+//EXEC FUNCTION
 int     	just_exec(t_sh *shell);
 int     	exec_cmd(t_ast *ast);
-int    		parser_execution(t_ast *ast);
+
+//EXPANSIONS
 ssize_t		apply_expansions(t_sh *shell);
 
-//exec.c
-void     do_pipe(t_ast *ast);
+//PARSE EXECUTION
+int    		parser_execution(t_ast *ast);
+
+//PARSER
+t_ast     *create_ast(t_ast *curr_node, t_ast *start, t_ast *end);
+
+//PIPE
+void     	do_pipe(t_ast *ast);
+
+//QUOTES
+void		handle_quotes(char **input);
+int     	is_valid_quotes(char *str);
+
+//REDIR_FUNCTIONS
+int		redir_great(t_ast *ast);
+int     redir_less(t_ast *ast);
+int     redir_dless(t_ast *ast);
+int     redir_and(t_ast *ast);
+
+//REDIR
 t_ast  *find_next_redir(t_ast *ast);
 int    exec_redirection(t_ast *ast);
 
 
+//ERROR CASE
+ssize_t     error_arg(void);
+int     	error_execution(char *cmd_name);
+void		error_option(char c);
+ssize_t		too_many_args(char *cmd);
+ssize_t		unvalid_setenv_cmd(void);
+ssize_t		error_chdir(int error, char *path, char *cmd);
+
+//==========================================================//
+
 //builtin
+int			find_builtin(char *cmd);
+int     	exec_builtin(t_sh *shell);
+
+//builtin minishell part added
 ssize_t		ms_echo(char **split_cmd, char ***ms_env, int ret);
 ssize_t		ms_cd(char **split_cmd, char ***ms_env, int ret);
 ssize_t		ms_setenv(char **split_cmd, char ***ms_env, int ret);
 ssize_t		ms_unsetenv(char **split_cmd, char ***ms_env, int ret);
 ssize_t		ms_env(char **split_cmd, char ***ms_env, int ret);
 ssize_t     ms_exit(char **split_cmd, char ***ms_env, int ret);
-int			find_builtin(char *cmd);
-
-//error
-void		error_option(char c);
-ssize_t		too_many_args(char *cmd);
-ssize_t		unvalid_setenv_cmd(void);
-ssize_t		error_chdir(int error, char *path, char *cmd);
-
 //utils2.c
 char		**find_first_bin(char **split_cmd, int c);
 size_t		getnbr_args(char **split_cmd, int c);
 void		copy_add_var_to_env(char ***env, char *name, char *value);
-
-//init.c
 ssize_t		exec_simple_env(char **cmd, char **s_bin, char **env);
 void	    init_opt_struct(t_opt *env_opt);
 void		init_env_options(char **split_cmd, t_opt *env_opt);
 void		valid_option(char c, t_opt *env_opt);
-//env.c
 void		add_argument_to_env(char **split_cmd, char ***env);
-
-// setenv.c
 char		**add_variable(char *v_name, char *v_value, char **ms_env);
-
-// redir
-int		redir_great(t_ast *ast);
-int     redir_less(t_ast *ast);
-int     redir_dless(t_ast *ast);
-int     redir_and(t_ast *ast);
-
-
-
-
 
 #endif
