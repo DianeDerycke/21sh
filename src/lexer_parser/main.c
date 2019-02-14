d00be74ef36d8d96a67364e1d6f7040b98638c28
DianeDerycke <dianedhayat@gmail.com>
2019-02-14 17:05:34 +0100

    remove printf, add ret handle error case

--- src/lexer_parser/main.c
+++ src/lexer_parser/main.c
@@ -6,7 +6,7 @@
 /*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/21 19:58:42 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/02/13 17:05:24 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2019/02/14 16:58:28 by DERYCKE          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,23 @@ int     main(void)
 {
 	t_param *param;
 	t_ast	*ast;
+	t_sh	*shell;
+	int		ret;
 
+	ret = 0;
 	while (21)
 	{
 		param = init_param();
 		param->input = get_valid_input();
+		shell = init_shell();
 		hy_history_write(param->input);
 		if (lex_input(param) == FAILURE)
 			printf("ERROR LEXER\n");
 		if (!(ast = create_ast(param->l_tokens, param->l_tokens, NULL)))
-			printf("ERROR DURING AST CREATION\n");
-		else if (parser_execution(ast) != 0)
-			printf("ERROR DURING AST EXECUTION\n");
+			ret = FAILURE;
+		else if (parser_execution(ast, shell) != 0)
+			ret = FAILURE;
 		free_param(param);
 	}
-	return (SUCCESS);
+	return (ret);
 }
