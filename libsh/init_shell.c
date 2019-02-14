54fa6b289f28edf96a025db5ccf9572465aade84
DianeDerycke <dianedhayat@gmail.com>
2019-02-14 17:04:00 +0100

    rename sh_get_shell to init_shell

--- libsh/init_shell.c
+++ libsh/init_shell.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sh_get_shell.c                                     :+:      :+:    :+:   */
+/*   init_shell.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 00:07:37 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/02/11 19:53:15 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2019/02/14 15:47:40 by DERYCKE          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libsh.h"
 
-t_sh     *sh_get_shell(t_ast *ast)
+t_sh     *init_shell(void)
 {
     t_sh *new;
 
     new = NULL;
     if (!(new = malloc(sizeof(t_sh))))
         ms_malloc_error();
-    if (!(new->cmd = sh_rtree_to_array(ast)))
-        return (NULL);
+    new->cmd = NULL;
     if (!(new->env = ms_get_env()))
         new->env = NULL;
     return (new);
