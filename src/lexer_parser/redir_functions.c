c59076e4da456a4f88cb09fe7208b73e825759c4
Diane DERYCKE <dideryck@student.42.fr>
2019-02-15 16:28:16 +0100

    add redir_dgreat && exit 1 if fd is -1

--- src/lexer_parser/redir_functions.c
+++ src/lexer_parser/redir_functions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redir_functions.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dideryck <dideryck@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 17:26:14 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/02/14 15:29:18 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2019/02/15 16:17:43 by dideryck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,26 @@
 int     redir_great(t_ast *ast)
 {
     int     fd;
-    if ((fd = open(ast->left->value,  O_RDWR | O_CREAT | O_TRUNC, 0777)))
+    if ((fd = open(ast->left->value,  O_RDWR | O_CREAT | O_TRUNC, 0677)))
     {
         dup2(fd, 1);
         close(fd);
+    }
+    return (SUCCESS);
+}
+
+int     redir_dgreat(t_ast *ast)
+{
+    int     fd;
+    if ((fd = open(ast->left->value, O_RDWR | O_CREAT | O_APPEND, 0677)))
+    {
+        dup2(fd, 1);
+        close(fd);
+    }
+    else if (fd == -1)
+    {
+        ms_no_such_file_or_dir("21sh", ast->left->value);
+        exit(1);
     }
     return (SUCCESS);
 }
@@ -31,8 +47,11 @@ int     redir_less(t_ast *ast)
         dup2(fd, 0);
         close(fd);
     }
-    else if (fd == -1)
+    else if (fd == ERROR)
+    {
+        ms_no_such_file_or_dir("21sh",ast->left->value);
         exit(1);
+    }
     return (SUCCESS);
 }
 
