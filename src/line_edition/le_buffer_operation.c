ed542e3f7ca1fe31c0ab4ac8c1382ae16da0d53d
Maxime RANDOU <mrandou@e1r8p13.42.fr>
2019-03-02 15:54:16 +0100

    fix: invalid read

--- src/line_edition/le_buffer_operation.c
+++ src/line_edition/le_buffer_operation.c
@@ -6,7 +6,7 @@
 /*   By: mrandou <mrandou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 17:47:26 by mrandou           #+#    #+#             */
-/*   Updated: 2019/03/02 15:12:07 by mrandou          ###   ########.fr       */
+/*   Updated: 2019/03/02 15:52:21 by mrandou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int		le_buff_append(struct s_le *le_struct, char c)
 			return (LE_FAILURE);
 	le_struct->cursor_x += 1;
 	le_struct->nb_char += 1;
+	le_struct->cursor_buff += 1;
 	if (le_struct->cursor_buff != le_struct->nb_char - 1)
 	{
 		if (le_window_clear_restore(le_struct))
@@ -96,7 +97,7 @@ int		le_buff_check_space(struct s_le *le_struct, int len)
 	if (le_struct->nb_char + len > le_struct->max_size)
 		return (LE_TRUNCATE);
 	if (le_struct->nb_char != LE_START\
-	&& le_struct->nb_char + len > le_struct->buffer_size)
+	&& le_struct->nb_char + len >= le_struct->buffer_size - 1)
 		return (LE_REALLOC);
 	return (0);
 }
@@ -110,17 +111,18 @@ int		le_buff_realloc(struct s_le *le_struct, int nb)
 {
 	char	*tmp;
 
-	le_struct->buffer_size += LE_BUFF_SIZE + nb;
+	le_struct->buffer_size += LE_BUFF_SIZE + nb + 1;
 	if (!(tmp = ft_strdup(le_struct->buff)))
 		return (LE_FAILURE);
 	ft_strdel(&le_struct->buff);
-	if (!(le_struct->buff = (char *)malloc(sizeof(char)\
+	if (!(le_struct->buff = (char *)malloc(sizeof(char)
 	* le_struct->buffer_size)))
 	{
 		ft_strdel(&tmp);
 		return (LE_FAILURE);
 	}
-	if (!(ft_strncpy(le_struct->buff, tmp, le_struct->buffer_size)))
+	ft_bzero(le_struct->buff, le_struct->buffer_size);
+	if (!(le_struct->buff = ft_strcpy(le_struct->buff, tmp)))
 	{
 		ft_strdel(&tmp);
 		return (LE_FAILURE);
