75ff205f98caba22fa80f71fffa2ccc21d41e16e
DianeDerycke <dianedhayat@gmail.com>
2019-03-04 17:20:52 +0100

    remove comment

--- src/builtins/init.c
+++ src/builtins/init.c
@@ -6,27 +6,12 @@
 /*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/12 12:44:35 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/02/13 12:51:21 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2019/03/04 17:20:40 by DERYCKE          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/sh21.h"
 
-// void	init_builtin_struct(t_builtin *builtin_tab)
-// {
-// 	ft_strcpy(builtin_tab[0].name, "echo");
-// 	builtin_tab[0].function = ms_echo;
-// 	ft_strcpy(builtin_tab[1].name, "cd");
-// 	builtin_tab[1].function = ms_cd;
-// 	ft_strcpy(builtin_tab[2].name, "setenv");
-// 	builtin_tab[2].function = ms_setenv;
-// 	ft_strcpy(builtin_tab[3].name, "unsetenv");
-// 	builtin_tab[3].function = ms_unsetenv;
-// 	ft_strcpy(builtin_tab[4].name, "env");
-// 	builtin_tab[4].function = ms_env;
-// 	ft_strcpy(builtin_tab[5].name, "help");
-// 	builtin_tab[5].function = ms_help;
-// }
 void		valid_option(char c, t_opt *env_opt)
 {
 	if (c == 'i')
