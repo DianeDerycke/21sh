64fcef8077b3076df42d85748ef20793a22b49c4
DianeDerycke <dianedhayat@gmail.com>
2019-03-04 18:29:18 +0100

    clear main and add process_input function

--- src/lexer_parser/main.c
+++ src/lexer_parser/main.c
@@ -6,16 +6,31 @@
 /*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/21 19:58:42 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/03/04 17:54:59 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2019/03/04 18:28:45 by DERYCKE          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/sh21.h"
 
+static int 	process_input(t_param *param, t_sh *shell)
+{
+	t_ast *ast;
+
+	ast = NULL;
+	// display_list(param->l_tokens);
+	// display_tree(ast, 0, 0);
+	if (lex_input(param) != SUCCESS)
+		return (FAILURE);
+	else if (!(ast = create_ast(param->l_tokens, param->l_tokens, NULL)))
+		return (FAILURE);
+	else if (ast && parser_execution(ast, shell) != 0)
+		return (FAILURE);
+	return (SUCCESS);
+}
+
 int     main(void)
 {
 	t_param *param;
-	t_ast	*ast;
 	t_sh	*shell;
 	int		ret;
 
@@ -26,18 +41,7 @@ int     main(void)
 		param = init_param();
 		param->input = get_valid_input(shell->env, ret);
 		hy_history_write(param->input, shell->env);
-		if ((ret = lex_input(param)) != SUCCESS)
-		{
-			free_param(param);
-			continue ;
-		}
-		// display_list(param->l_tokens);
-		ret = SUCCESS;
-		if (!(ast = create_ast(param->l_tokens, param->l_tokens, NULL)))
-			ret = FAILURE;
-		// display_tree(ast, 0, 0);
-		if (ast && parser_execution(ast, shell) != 0)
-			ret = FAILURE;
+		ret = process_input(param, shell);
 		free_param(param);
 		if (!isatty(0))
 			break;
