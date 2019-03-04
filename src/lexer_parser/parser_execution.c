3375ab4f8f9599e7999e1eed82f39af9163ec7cd
DianeDerycke <dianedhayat@gmail.com>
2019-03-04 16:53:56 +0100

    get return recurse_pipe, add function to free list of pid

--- src/lexer_parser/parser_execution.c
+++ src/lexer_parser/parser_execution.c
@@ -6,7 +6,7 @@
 /*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 13:41:10 by dideryck          #+#    #+#             */
-/*   Updated: 2019/03/01 13:36:45 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2019/03/04 15:58:57 by DERYCKE          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,11 @@ int    parser_execution(t_ast *ast, t_sh *shell)
     if (ast->token == SEPARATOR && ast->right)
         ret = parser_execution(ast->right, shell);
     else if (ast->token == PIPE)
-        recurse_pipe(shell, ast, NULL);
+    {
+        ret = recurse_pipe(shell, ast, NULL);
+        sh_freepidlist(shell->l_pid);
+        shell->l_pid = NULL;
+    }
     else if (ast->token >= WORD)
     {
         if (find_next_redir(ast))
