a08a94925b0e0d2dec074ed79923fdb675fe3936
DianeDerycke <dianedhayat@gmail.com>
2019-03-04 16:54:45 +0100

    fix ctrl + c signal && return procc termination

--- src/lexer_parser/pipe.c
+++ src/lexer_parser/pipe.c
@@ -6,30 +6,21 @@
 /*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 23:11:10 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/03/01 16:29:35 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2019/03/04 16:38:36 by DERYCKE          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/sh21.h"
 
-static void		close_pipe(int *fd)
+static int		handle_right_command(int *oldfd, int *newfd, t_sh *shell, t_ast *ast)
 {
-	if (!fd)
-		return ;
-	close(fd[INPUT_END]);
-	close(fd[OUTPUT_END]);
+	if (oldfd)
+		close_dup(oldfd, STDIN_FILENO);
+	close_dup(newfd, STDOUT_FILENO);
+	return (exec_pipe_cmd(shell, ast));
 }
 
-static void		close_dup(int *fd, int std)
-{
-	if (!fd)
-		return ;
-	close(fd[std == STDIN_FILENO ? INPUT_END : OUTPUT_END]);
-	dup2(fd[std == STDIN_FILENO ? OUTPUT_END : INPUT_END], std);
-	close(fd[std == STDIN_FILENO ? OUTPUT_END : INPUT_END]);
-}
-
-static void		end_recurse_pipe(t_sh *shell, t_ast *ast, int *oldfd, int *newfd)
+static int		end_recurse_pipe(t_sh *shell, t_ast *ast, int *oldfd, int *newfd)
 {
 	pid_t	child_pid;
 	int		status;
@@ -39,48 +30,44 @@ static void		end_recurse_pipe(t_sh *shell, t_ast *ast, int *oldfd, int *newfd)
 	if (child_pid == 0)
 	{
 		close_dup(newfd, STDIN_FILENO);
-		if (oldfd)
-			close_pipe(oldfd);
-		exec_pipe_cmd(shell, ast);
+		close_pipe(oldfd);
+		return (exec_pipe_cmd(shell, ast));
 	}
 	else
 	{
-		if (oldfd)
-			close_pipe(oldfd);
+		close_pipe(oldfd);
 		close_pipe(newfd);
-		get_pid(child_pid);
+		sh_push_pidnew(child_pid, &(shell->l_pid));
+		get_pid_list(shell->l_pid);
 		signal(SIGINT, signal_handler);
 		waitpid(child_pid, &status, 0);
 	}
+	return (status);
 }
 
-void		recurse_pipe(t_sh *shell, t_ast *ast, int *oldfd)
+int				recurse_pipe(t_sh *shell, t_ast *ast, int *oldfd)
 {
 	int		newfd[2];
 	pid_t	child_pid;
 	int		status;
+	int		ret;
 
-	status = 0;
-	if (pipe(newfd) == -1)
-		exit (1);
-	child_pid = fork();
+	ret = 0;
+	if ((pipe(newfd) == -1) || ((child_pid = fork()) < 0))
+		return (ERROR);
 	if (child_pid == 0)
-	{
-		if (oldfd)	
-			close_dup(oldfd, STDIN_FILENO);
-		close_dup(newfd, STDOUT_FILENO);
-		exec_pipe_cmd(shell, ast->right);
-	}
+		ret = handle_right_command(oldfd, newfd, shell, ast->right);
 	else
 	{
-		if (oldfd)
-			close_pipe(oldfd);
-		get_pid(child_pid);
+		close_pipe(oldfd);
+		if (ast->left && ast->left->token == PIPE)
+			ret = recurse_pipe(shell, ast->left, newfd);
+		else if (ast->left)
+			ret = end_recurse_pipe(shell, ast->left, oldfd, newfd);
+		sh_push_pidnew(child_pid, &(shell->l_pid));
+		get_pid_list(shell->l_pid);
 		signal(SIGINT, signal_handler);
 		waitpid(child_pid, &status, 0);
-		if (ast->left && ast->left->token == PIPE)
-			recurse_pipe(shell, ast->left, newfd);
-		else if (ast->left)
-			end_recurse_pipe(shell, ast->left, oldfd, newfd);
 	}
+	return (ret);
 }
