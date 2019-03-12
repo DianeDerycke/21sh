a74c7e59b253c36c1ed59a5dbe7dd6c73a23808b
DianeDerycke <dianedhayat@gmail.com>
2019-03-12 10:07:19 +0100

    handling heredoc during pipe

--- src/lexer_parser/pipe.c
+++ src/lexer_parser/pipe.c
@@ -6,24 +6,51 @@
 /*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 23:11:10 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/03/08 12:51:08 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2019/03/12 10:05:45 by DERYCKE          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/sh21.h"
 
+static int		handle_heredoc_pipe(t_ast *ast)
+{
+	t_ast 	*redir;
+	t_ast 	*tmp;
+
+	tmp = ast;
+	while ((redir = find_next_redir(tmp)))
+	{
+		if (redir->token == DLESS)
+			return (SUCCESS);
+		tmp = tmp->left;
+	}
+	return (FAILURE);
+}
+
 static int		handle_right_command(t_sh *shell, t_ast *ast, int *oldfd, int *newfd)
 {
-	if (oldfd)
-		close_dup(oldfd, STDIN_FILENO);
-	close_dup(newfd, STDOUT_FILENO);
+	if (handle_heredoc_pipe(ast) == SUCCESS)
+	{
+		if (oldfd)
+			close_pipe(oldfd);
+		close_dup(newfd, STDOUT_FILENO);
+	}
+	else
+	{
+		if (oldfd)
+			close_dup(oldfd, STDIN_FILENO);
+		close_dup(newfd, STDOUT_FILENO);
+	}
 	return (exec_pipe_cmd(shell, ast));
 }
 
 static int		end_recurse_pipe(t_sh *shell, t_ast *ast, int *oldfd, int *newfd)
 {
-	close_dup(newfd, STDIN_FILENO);
 	close_pipe(oldfd);
+	if (handle_heredoc_pipe(ast) == SUCCESS)
+		close_pipe(newfd);
+	else
+		close_dup(newfd, STDIN_FILENO);
 	return (exec_pipe_cmd(shell, ast));
 }
 
