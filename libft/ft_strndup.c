0d84c44e9c83187a9ab6dc70669853a9ddc118a8
DianeDerycke <dianedhayat@gmail.com>
2019-03-13 14:07:56 +0100

    add memalloc instead of simple malloc

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strndup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dideryck <dideryck@student.42.fr>          +#+  +:+       +#+        */
+/*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 00:40:55 by dideryck          #+#    #+#             */
-/*   Updated: 2017/11/30 00:49:50 by dideryck         ###   ########.fr       */
+/*   Updated: 2019/03/13 14:07:39 by DERYCKE          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ char	*ft_strndup(const char *s1, size_t len)
 	char	*cpy;
 
 	i = 0;
-	cpy = (char *)malloc((len + 1) * (sizeof(char)));
+	cpy = ft_memalloc(len + 1);
+	// cpy = (char *)malloc((len + 1) * (sizeof(char)));
 	if (cpy == NULL)
 		return (NULL);
 	while (i < len && s1[i])
