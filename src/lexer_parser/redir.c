ff6e9f5a1735f1b60135b9bca5d58f01895a116b
Diane DERYCKE <dideryck@student.42.fr>
2019-03-14 13:37:56 +0100

    add loop into static function

--- src/lexer_parser/redir.c
+++ src/lexer_parser/redir.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redir.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dideryck <dideryck@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 23:10:08 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/03/13 13:36:28 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2019/03/14 13:34:17 by dideryck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,36 @@ void    reset_std(int *fd)
 	close(fd[INPUT_END]);
 }
 
-static int     open_file(t_ope token, char *file, t_sh *shell)
+static int     open_file(t_ope token, char *file)
 {
     if (token == GREAT)
         return (open(file, O_RDWR | O_CREAT | O_TRUNC, PERM));
     else if (token == DGREAT)
         return (open(file, O_RDWR | O_CREAT | O_APPEND, PERM));
     else if (token == DLESS)
-        return (handle_heredoc(file, shell));
+        return (handle_heredoc(file));
     else
         return (open(file, O_RDWR));
 }
 
-int     exec_redirection(t_ast *ast, t_sh *shell)
+static int      do_redirection(t_ast *redir)
 {
     int     fd;
+
+    if ((fd = open_file(redir->token, redir->left->value)) < 0)
+    {
+        reset_std(getter_std(0));
+        if (fd == -2)
+            return (FAILURE);
+        return (get_error(NOFILEDIR, redir->left->value));
+    }
+    dup2(fd, get_str_redir(redir->token));
+    close(fd);
+    return (SUCCESS);
+}
+
+int     exec_redirection(t_ast *ast, t_sh *shell)
+{
     t_ast    *cmd;
     t_ast   *redir;
     t_ast   *tmp;
@@ -58,24 +73,15 @@ int     exec_redirection(t_ast *ast, t_sh *shell)
     cmd = NULL;
     while ((redir = find_next_redir(tmp)))
     {
-        if ((fd = open_file(redir->token, redir->left->value, shell)) < 0)
-        {
-            reset_std(getter_std(0));
-            if (fd == -2)
-                return (FAILURE);
-            return (get_error(NOFILEDIR, redir->left->value));
-        }
-        else
-        {
-            dup2(fd, get_str_redir(redir->token));
-            close(fd);
-        }
+        if (do_redirection(redir) == FAILURE)
+            return (FAILURE);
         tmp = redir->left;
     }
     if (!shell->heredoc && (cmd = add_argument_to_cmd(ast)))
+    {
         ret = exec_cmd(cmd, shell);
-    reset_std(getter_std(0));
-    if (cmd)
         free_ast(&cmd);
+    }
+    reset_std(getter_std(0));
     return (ret);
 }
