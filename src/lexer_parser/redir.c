a89a8ea6867f5c8c8381f7786399f0f6355a1f58
Diane DERYCKE <dideryck@student.42.fr>
2019-03-14 16:30:53 +0100

    add agregation feature

--- src/lexer_parser/redir.c
+++ src/lexer_parser/redir.c
@@ -6,7 +6,7 @@
 /*   By: dideryck <dideryck@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 23:10:08 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/03/14 15:08:41 by dideryck         ###   ########.fr       */
+/*   Updated: 2019/03/14 16:29:55 by dideryck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void    reset_std(int *fd)
 	close(fd[INPUT_END]);
 }
 
-static int     open_file(t_ope token, char *file, t_ast *redir)
+static int     open_file(t_ope token, char *file, t_ast *redir, t_ast *ast)
 {
     (void)redir;
     if (token == GREAT)
@@ -42,23 +42,28 @@ static int     open_file(t_ope token, char *file, t_ast *redir)
         return (open(file, O_RDWR | O_CREAT | O_APPEND, PERM));
     else if (token == DLESS)
         return (handle_heredoc(redir));
+    else if (token == LESSAND || token == GREATAND)
+        return (handle_agregation(redir, ast));
     else
         return (open(file, O_RDWR));
 }
 
-static int      do_redirection(t_ast *redir)
+static int      do_redirection(t_ast *redir, t_ast *ast)
 {
     int         fd;
 
-    if ((fd = open_file(redir->token, redir->left->value, redir)) < 0)
+    if ((fd = open_file(redir->token, redir->left->value, redir, ast)) < 0)
     {
         reset_std(getter_std(0));
         if (fd == -2)
             return (FAILURE);
         return (get_error(NOFILEDIR, redir->left->value));
     }
-    dup2(fd, get_str_redir(redir->token));
-    close(fd);
+    if (redir->token != LESSAND && redir->token != GREATAND)
+    {
+        dup2(fd, get_str_redir(redir->token));
+        close(fd);
+    }
     return (SUCCESS);
 }
 
@@ -69,20 +74,19 @@ int     exec_redirection(t_ast *ast, t_sh *shell)
     t_ast   *tmp;
     int     ret;
 
-    ret = 0;
-    tmp = ast;
     cmd = NULL;
+    tmp = ast;
+    ret = 0;
     while ((redir = find_next_redir(tmp)))
     {
-        if (do_redirection(redir) == FAILURE)
+        if (do_redirection(redir, ast) == FAILURE)
             return (FAILURE);
         tmp = redir->left;
     }
     if ((cmd = add_argument_to_cmd(ast)))
-    {
         ret = exec_cmd(cmd, shell);
+    if (cmd)
         free_ast(&cmd);
-    }
     reset_std(getter_std(0));
     return (ret);
 }
