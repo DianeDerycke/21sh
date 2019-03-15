57b29e1dbe868cfbbbaf1bcc93b0ac3af42fdf07
Diane DERYCKE <dideryck@student.42.fr>
2019-03-15 15:58:29 +0100

    edit return pipe

--- src/lexer_parser/pipe.c
+++ src/lexer_parser/pipe.c
@@ -6,7 +6,7 @@
 /*   By: dideryck <dideryck@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 23:11:10 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/03/14 15:25:51 by dideryck         ###   ########.fr       */
+/*   Updated: 2019/03/15 15:58:23 by dideryck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,6 @@ static int				recurse_pipe(t_sh *shell, t_ast *ast, int *oldfd, int *fd)
 
 	status = 0;
 	ret = 0;
-	getter_std(1);
 	if (!fd)
 		if (pipe(newfd) == ERROR)
 			return (ERROR);
@@ -72,7 +71,7 @@ static int				recurse_pipe(t_sh *shell, t_ast *ast, int *oldfd, int *fd)
 	if (child_pid == 0)
 	{
 		if (fd)
-			end_recurse_pipe(shell, ast, oldfd, fd);
+			ret = end_recurse_pipe(shell, ast, oldfd, fd);
 		else
 			handle_right_command(shell, ast->right, oldfd, newfd);
 	}
@@ -81,15 +80,15 @@ static int				recurse_pipe(t_sh *shell, t_ast *ast, int *oldfd, int *fd)
 		close_pipe(fd);
 		close_pipe(oldfd);
 		if (ast->left && ast->left->token == PIPE)
-			ret = recurse_pipe(shell, ast->left, newfd, NULL);
+			recurse_pipe(shell, ast->left, newfd, NULL);
 		else if (ast->left && ast->token == PIPE)
-			ret = recurse_pipe(shell, ast->left, oldfd, newfd);
+			recurse_pipe(shell, ast->left, oldfd, newfd);
 		sh_push_pidnew(child_pid, &(shell->l_pid));
 		get_pid_list(shell->l_pid);
 		signal(SIGINT, signal_handler);
 		waitpid(child_pid, &status, 0);
 	}
-	return (SUCCESS);
+	return (ret);
 }
 
 int 	do_pipe(t_sh *shell, t_ast *ast)
