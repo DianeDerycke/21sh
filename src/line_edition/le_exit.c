468f0f792816475bde8382f1b12721b1705d30ac
Maxime RANDOU <mrandou@e1r5p13.42.fr>
2019-03-15 12:04:59 +0100

    edit: backslash prompt codition

--- src/line_edition/le_exit.c
+++ src/line_edition/le_exit.c
@@ -6,7 +6,7 @@
 /*   By: mrandou <mrandou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 16:48:36 by mrandou           #+#    #+#             */
-/*   Updated: 2019/03/12 18:04:11 by mrandou          ###   ########.fr       */
+/*   Updated: 2019/03/15 12:02:00 by mrandou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ int		le_exit(struct s_le *le_struct)
 	}
 	if ((le_struct->term == LE_EOF || le_struct->term == LE_ETX)
 	&& (le_struct->prompt_type == SQUOTE || le_struct->prompt_type == DQUOTE
-	|| le_struct->prompt_type == HEREDOC || le_struct->prompt_type == IS_PIPE))
+	|| le_struct->prompt_type == HEREDOC || le_struct->prompt_type == IS_PIPE
+	|| le_struct->prompt_type == BACKSLASH))
 	{
 		le_struct->buff = ft_strcpy(le_struct->buff, 
 		le_struct->term == LE_EOF ? "\004" : "\003");
