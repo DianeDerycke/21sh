06e7d7374dc8d4c1c4c69861956d1f9a73c359db
Maxime RANDOU <mrandou@e1r5p13.42.fr>
2019-03-15 12:13:43 +0100

    edit: backslash prompt condition 2

--- src/line_edition/le_prompt.c
+++ src/line_edition/le_prompt.c
@@ -6,7 +6,7 @@
 /*   By: mrandou <mrandou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 16:44:04 by mrandou           #+#    #+#             */
-/*   Updated: 2019/03/15 11:53:18 by mrandou          ###   ########.fr       */
+/*   Updated: 2019/03/15 12:13:22 by mrandou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int		le_prompt_init(struct s_le *le_struct, char **env)
 	if (le_prompt_get_color(le_struct, env))
 		ft_strcpy(le_struct->prompt_color, LE_PROMPT_DFLT_CLR);
 	if (le_struct->prompt_type != DQUOTE && le_struct->prompt_type != SQUOTE
-	&& le_struct->prompt_type != IS_PIPE && le_struct->prompt_type != HEREDOC)
+	&& le_struct->prompt_type != IS_PIPE && le_struct->prompt_type != HEREDOC
+	&& le_struct->prompt_type != BACKSLASH)
 	{
 		if (le_prompt_pwd(le_struct, env))
 		{
