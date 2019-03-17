6dfdaa8822f1546d15ebd17fb9164fef3bb1d202
DianeDerycke <dianedhayat@gmail.com>
2019-03-17 03:49:33 +0100

    delete is_command && treat_command proto

--- includes/execution.h
+++ includes/execution.h
@@ -6,7 +6,7 @@
 /*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/17 02:33:26 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/03/17 02:39:41 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2019/03/17 03:48:19 by DERYCKE          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 # include "../libsh/libsh.h"
 
-int      	is_command(t_sh *shell);
-int    		treat_command(t_sh *shell, t_ast *ast);
 int     	exec_cmd(t_ast *ast, t_sh *shell);
 int     	exec_pipe_cmd(t_sh *shell, t_ast *ast);
 int    		parser_execution(t_ast *ast, t_sh *shell);
