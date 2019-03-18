2bdac4ee6b324c9f5051607c2240dc8798dcf0ce
Diane DERYCKE <dideryck@student.42.fr>
2019-03-18 16:23:42 +0100

    remove global

--- src/builtins/builtin.c
+++ src/builtins/builtin.c
@@ -6,46 +6,57 @@
 /*   By: dideryck <dideryck@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 23:12:17 by DERYCKE           #+#    #+#             */
-/*   Updated: 2019/03/18 14:49:11 by dideryck         ###   ########.fr       */
+/*   Updated: 2019/03/18 16:21:37 by dideryck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/builtins.h"
 
-t_builtin	builtin_array[20] = {
-	{"echo", ms_echo},
-	{"cd", ms_cd},
-	{"setenv", ms_setenv},
-	{"unsetenv", ms_unsetenv},
-	{"env", ms_env},
-	{"exit", ms_exit},
-};
-
-int		find_builtin(char *cmd)
+t_builtin		getter_builtin(int index)
 {
-	int		i;
+	static t_builtin	builtin_array[20] = {
+		{"echo", ms_echo},
+		{"cd", ms_cd},
+		{"setenv", ms_setenv},
+		{"unsetenv", ms_unsetenv},
+		{"env", ms_env},
+		{"exit", ms_exit},
+	};
+
+	return (builtin_array[index]);
+}
+
+int				find_builtin(char *cmd)
+{
+	int			i;
+	t_builtin	builtin;
 
 	i = 0;
 	if (!cmd)
 		return (-1);
 	while (i < BUILTIN_SIZE)
 	{
-		if (ft_strcmp(cmd, builtin_array[i].name) == SUCCESS)
+		builtin = getter_builtin(i);
+		if (ft_strcmp(cmd, builtin.name) == SUCCESS)
 			return (i);
 		i++;
 	}
 	return (ERROR);
 }
 
-int		exec_builtin(t_sh *shell)
+int				exec_builtin(t_sh *shell)
 {
-	int		index;
-	int		ret;
+	int			index;
+	int			ret;
+	t_builtin	builtin;
 
 	ret = 0;
 	index = 0;
 	if ((index = find_builtin(shell->cmd[0])) >= 0)
-		ret = builtin_array[index].function(shell->cmd, &(shell->env), 0);
+	{
+		builtin = getter_builtin(index);
+		ret = builtin.function(shell->cmd, &(shell->env), 0);
+	}
 	ft_free_array(shell->cmd);
 	free(shell->cmd);
 	shell->cmd = NULL;
